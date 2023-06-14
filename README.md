58272b4ddb424a7ad35480f11c39661eb4ab54b8
Antoine <antoine.gagniere@orolia2s.com>
2023-06-14 19:24:03 +0200

    Initial commit: Readme and interface skeleton

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lib O2S
+
+Reliant datastructure library for C.
+
+The [include](include) folder contains the public interface, while the [src](src) folder contains the implementation.
---
+++ include/o2s/array.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef struct array
+{
+} array_t;
+
+#define Array(T) array_new(sizeof(T))
+
+array_t array_new(size_t type_size);
+
+bool array_push_back(array_t* self, void* element);
+bool array_push_back_n(array_t* self, void* elements, size_t count);
+
+bool array_pop_back(array_t* self, void* destination);
+bool array_pop_back_n(array_t* self, void* destination, size_t count);
+
+bool array_reserve(array_t* self, size_t count);
+bool array_clear(array_t* self);
