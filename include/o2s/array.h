217ec5e1837321ec428d7569df868356b9aca4ab
Antoine <antoine.gagniere@orolia2s.com>
2023-06-20 10:53:05 +0200

    Add getters and clang format

--- include/o2s/array.h
+++ include/o2s/array.h
@@ -1,5 +1,19 @@
 #pragma once
 
+/* ************************************************************************** */
+/*      ____    _    _____ ____     _    _   _   _____ _  _   ____  ____      */
+/*     / ___|  / \  |  ___|  _ \   / \  | \ | | |_   _| || | |  _ \/ ___|     */
+/*     \___ \ / _ \ | |_  | |_) | / _ \ |  \| |   | | | || |_| | | \___ \     */
+/*      ___) / ___ \|  _| |  _ < / ___ \| |\  |   | | |__   _| |_| |___) |    */
+/*     |____/_/   \_|_|   |_| \_/_/   \_|_| \_|   |_|    |_| |____/|____/     */
+/*                                                                            */
+/* Copyright 2023, SAFRAN T4DS, ALL RIGHTS RESERVED                           */
+/*                                                                            */
+/* @file array.h                                                              */
+/* @author Antoine GAGNIERE                                                   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdbool.h>
 #include <stddef.h>
 
@@ -11,11 +25,15 @@ typedef struct array
 
 array_t array_new(size_t type_size);
 
-bool array_push_back(array_t* self, void* element);
-bool array_push_back_n(array_t* self, void* elements, size_t count);
+bool    array_push_back(array_t* self, void* element);
+bool    array_push_back_n(array_t* self, void* elements, size_t count);
 
-bool array_pop_back(array_t* self, void* destination);
-bool array_pop_back_n(array_t* self, void* destination, size_t count);
+bool    array_pop_back(array_t* self, void* destination);
+bool    array_pop_back_n(array_t* self, void* destination, size_t count);
 
-bool array_reserve(array_t* self, size_t count);
-bool array_clear(array_t* self);
+void*   array_first(array_t* self);
+void*   array_last(array_t* self);
+void*   array_get(array_t* self, size_t index);
+
+bool    array_reserve(array_t* self, size_t count);
+bool    array_clear(array_t* self);
