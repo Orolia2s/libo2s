88c94a415654bbaf54a31c6ca212f57b49759ac3
Antoine <antoine.gagniere@orolia2s.com>
2023-07-19 16:17:11 +0200

    Allow log level customization

--- include/o2s/log.h
+++ include/o2s/log.h
@@ -2,11 +2,39 @@
 
 #include <stdio.h> // fprintf, stdout, stderr
 
-#define log_log(FILE, LEVEL, COLOR, FORMAT, ...) fprintf(FILE, "[\e[" COLOR "m" LEVEL "\e[0m] " FORMAT "\n" , ## __VA_ARGS__ )
+/*
+#define LOG_LEVEL_FATAL   0
+#define LOG_LEVEL_ERROR   1
+*/
+#define LOG_LEVEL_WARNING 2
+#define LOG_LEVEL_INFO    3
+#define LOG_LEVEL_DEBUG   4
+#define LOG_LEVEL_TRACE   5
 
-#define log_trace(...) log_log(stdout, "TRACE", "0", __VA_ARGS__)
+#ifndef LOG_LEVEL
+# define LOG_LEVEL LOG_LEVEL_INFO
+#endif
+
+#define log_log(FILE, LEVEL, COLOR, FORMAT, ...) fprintf(FILE, "[\e[" COLOR "m" LEVEL "\e[0m] " __FILE__ " " FORMAT "\n" , ## __VA_ARGS__ )
+
+#if LOG_LEVEL >= LOG_LEVEL_TRACE
+# define log_trace(...) log_log(stdout, "TRACE", "0", __VA_ARGS__)
+#else
+# define log_trace(...)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_DEBUG
 #define log_debug(...) log_log(stdout, "DEBUG", "34", __VA_ARGS__)
-#define log_info(...)  log_log(stdout, "INFO ", "1;35", __VA_ARGS__)
+#else
+#define log_debug(...)
+#endif
+
+#if LOG_LEVEL >= LOG_LEVEL_INFO
+# define log_info(...)  log_log(stdout, "INFO ", "1;35", __VA_ARGS__)
+#else
+# define log_info(...)
+#endif
+
 #define log_warning(...) log_log(stderr, "WARN ", "1;33", __VA_ARGS__)
 #define log_error(...) log_log(stderr, "ERROR", "1;31", __VA_ARGS__)
 #define log_fatal(...) log_log(stderr, "FATAL", "1;97;41", __VA_ARGS__)
