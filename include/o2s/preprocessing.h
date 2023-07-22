30faafc6371f81c2c2a61cb4b96c036d9ec7787e
Antoine <antoine.gagniere@orolia2s.com>
2023-07-22 15:01:55 +0200

    Add a function to display library version

---
+++ include/o2s/preprocessing.h
@@ -0,0 +1,21 @@
+#pragma once
+
+/* ************************************************************************** */
+/*      ____    _    _____ ____     _    _   _   _____ _  _   ____  ____      */
+/*     / ___|  / \  |  ___|  _ \   / \  | \ | | |_   _| || | |  _ \/ ___|     */
+/*     \___ \ / _ \ | |_  | |_) | / _ \ |  \| |   | | | || |_| | | \___ \     */
+/*      ___) / ___ \|  _| |  _ < / ___ \| |\  |   | | |__   _| |_| |___) |    */
+/*     |____/_/   \_|_|   |_| \_/_/   \_|_| \_|   |_|    |_| |____/|____/     */
+/*                                                                            */
+/* Copyright 2023, SAFRAN T4DS, ALL RIGHTS RESERVED                           */
+/*                                                                            */
+/* @file preprocessing.h                                                      */
+/* @author Antoine GAGNIERE                                                   */
+/*                                                                            */
+/* ************************************************************************** */
+
+/** Changes token type to string literal */
+#define LIBO2S_PREPRO_STRING_DIRECT(EXPRESSION) # EXPRESSION
+
+/** Create a string literal from the evaluation of its argument */
+#define LIBO2S_PREPRO_STRING(EXPRESSION) LIBO2S_PREPRO_STRING_DIRECT(EXPRESSION)
---
+++ src/version.c
@@ -0,0 +1,7 @@
+#include "o2s/version.h"
+
+/** The libO2S version as a null-terminated string. */
+const char* libo2s_version_cstr()
+{
+	return LIBO2S_PREPRO_STRING(LIBO2S_VERSION);
+}
