3accd0a8a16ddb4258de7cad145ee52358d152a9
Antoine <antoine.gagniere@orolia2s.com>
2023-07-31 14:04:23 +0200

    Add 2 deque aliases: queue and stack

---
+++ include/o2s/queue.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/* ************************************************************************** */
+/*      ____    _    _____ ____     _    _   _   _____ _  _   ____  ____      */
+/*     / ___|  / \  |  ___|  _ \   / \  | \ | | |_   _| || | |  _ \/ ___|     */
+/*     \___ \ / _ \ | |_  | |_) | / _ \ |  \| |   | | | || |_| | | \___ \     */
+/*      ___) / ___ \|  _| |  _ < / ___ \| |\  |   | | |__   _| |_| |___) |    */
+/*     |____/_/   \_|_|   |_| \_/_/   \_|_| \_|   |_|    |_| |____/|____/     */
+/*                                                                            */
+/**
+ * @copyright 2023, SAFRAN T4DS, ALL RIGHTS RESERVED
+ * @file queue.h
+ * @author Antoine GAGNIERE
+ * @brief Realtime FIFO buffer
+ */
+/* ************************************************************************** */
+
+typedef deque_t queue_t;
+
+#define queue_push   deque_push_back
+#define queue_push_n deque_push_back_n
+
+#define queue_pop    deque_pop_front
+#define queue_pop_n  deque_pop_front_n
---
+++ include/o2s/stack.h
@@ -0,0 +1,24 @@
+#pragma once
+
+/* ************************************************************************** */
+/*      ____    _    _____ ____     _    _   _   _____ _  _   ____  ____      */
+/*     / ___|  / \  |  ___|  _ \   / \  | \ | | |_   _| || | |  _ \/ ___|     */
+/*     \___ \ / _ \ | |_  | |_) | / _ \ |  \| |   | | | || |_| | | \___ \     */
+/*      ___) / ___ \|  _| |  _ < / ___ \| |\  |   | | |__   _| |_| |___) |    */
+/*     |____/_/   \_|_|   |_| \_/_/   \_|_| \_|   |_|    |_| |____/|____/     */
+/*                                                                            */
+/**
+ * @copyright 2023, SAFRAN T4DS, ALL RIGHTS RESERVED
+ * @file stack.h
+ * @author Antoine GAGNIERE
+ * @brief Realtime LIFO buffer
+ */
+/* ************************************************************************** */
+
+typedef deque_t stack_t;
+
+#define stack_push   deque_push_back
+#define stack_push_n deque_push_back_n
+
+#define stack_pop    deque_pop_back
+#define stack_pop_n  deque_pop_back_n
