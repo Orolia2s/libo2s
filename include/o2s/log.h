5944d61c229204a22fa71e92b1b860f2925bf50e
Antoine <antoine.gagniere@orolia2s.com>
2023-12-01 18:12:37 +0100

    Log output file is now custmizable at compile-time

--- include/o2s/log.h
+++ include/o2s/log.h
@@ -32,24 +32,29 @@
 /** Set this value to control the logs verbosity */
 #	define LOG_LEVEL LOG_LEVEL_INFO
 #endif
+
+#ifndef LOG_OUTPUT_FILE
+/** Choose the file in which log lines are written */
+#	define LOG_OUTPUT_FILE stderr
+#endif
 ///@}
 
-#define log_log(FILE, LEVEL, COLOR, FORMAT, ...) \
-	fprintf(FILE, \
+#define log_log(LEVEL, COLOR, FORMAT, ...) \
+	fprintf(LOG_OUTPUT_FILE, \
 	        "[\e[" COLOR "m" LEVEL "\e[0m]\e[2m " __FILE__ ":%s:%02i: \e[0m" FORMAT "\n", \
 	        __FUNCTION__, \
 	        __LINE__, \
 	        ##__VA_ARGS__)
 
 #if LOG_LEVEL >= LOG_LEVEL_TRACE
-#	define log_trace(...) log_log(stdout, "TRACE", "0", __VA_ARGS__)
+#	define log_trace(...) log_log("TRACE", "0", __VA_ARGS__)
 #else
 /** Report information of the lowest importance */
 #	define log_trace(...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_DEBUG
-#	define log_debug(...) log_log(stdout, "DEBUG", "34", __VA_ARGS__)
+#	define log_debug(...) log_log("DEBUG", "34", __VA_ARGS__)
 #else
 /** Report information useful to the developer */
 #	define log_debug(...)
@@ -57,28 +62,28 @@
 
 #if LOG_LEVEL >= LOG_LEVEL_INFO
 /** Report a significant event */
-#	define log_info(...) log_log(stdout, "INFO ", "1;32", __VA_ARGS__)
+#	define log_info(...) log_log("INFO ", "1;32", __VA_ARGS__)
 #else
 #	define log_info(...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_WARNING
 /** Report an usual condition, not significant enough to abort the current operation */
-#	define log_warning(...) log_log(stderr, "WARN ", "1;33", __VA_ARGS__)
+#	define log_warning(...) log_log("WARN ", "1;33", __VA_ARGS__)
 #else
 #	define log_warning(...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_ERROR
 /** Report a condition causing the current operation to abort */
-#	define log_error(...) log_log(stderr, "ERROR", "1;31", __VA_ARGS__)
+#	define log_error(...) log_log("ERROR", "1;31", __VA_ARGS__)
 #else
 #	define log_error(...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_FATAL
 /** Report a condition causing the program to halt */
-#	define log_fatal(...) log_log(stderr, "FATAL", "1;97;41", __VA_ARGS__)
+#	define log_fatal(...) log_log("FATAL", "1;97;41", __VA_ARGS__)
 #else
 #	define log_fatal(...)
 #endif
