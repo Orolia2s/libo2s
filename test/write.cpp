b6d1d6e8bbf8d7994b7ba44c293e358cbf14550f
Antoine <antoine.gagniere@orolia2s.com>
2024-03-07 10:54:54 +0100

    Test write

---
+++ test/write.cpp
@@ -0,0 +1,54 @@
+extern "C"
+{
+#include "o2s/write.h"
+
+#include "o2s/deque.h"
+#include "o2s/file_input_stream.h"
+#include "o2s/log.h"
+#include "o2s/string.h"
+
+#include <fcntl.h> // O_RDONLY
+
+#include <stdio.h>  // tmpfile, fileno
+#include <unistd.h> // open, write, read
+}
+
+#include <catch2/catch_test_macros.hpp>
+
+SCENARIO("We can write to a file descriptor with guarantees", "[write]")
+{
+	GIVEN("A huge number of bytes")
+	{
+		size_t  length   = 8'000'000;
+		int     random   = open("/dev/random", O_RDONLY);
+		String  to_write = string_new();
+		ssize_t was_read;
+
+		REQUIRE(string_reserve(&to_write, length));
+		was_read = read(random, to_write.start, to_write.capacity);
+		REQUIRE(was_read > 0);
+		to_write.count = was_read;
+
+		THEN("We can write it with a single call")
+		{
+			FILE*   out = tmpfile();
+			ssize_t written;
+
+			written = write(fileno(out), (char*)to_write.start, to_write.count);
+			REQUIRE(written > 0);
+			CHECK(written == to_write.count);
+
+			AND_THEN("The right bytes were written")
+			{
+				String was_written = string_new();
+
+				REQUIRE(string_reserve(&was_written, written));
+				rewind(out);
+				was_read = read(fileno(out), (char*)was_written.start, was_written.capacity);
+				REQUIRE(was_read > 0);
+				was_written.count = was_read;
+				CHECK(string_is_equal(&to_write, &was_written));
+			}
+		}
+	}
+}
