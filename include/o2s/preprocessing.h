22487daf6919509bf08519ec13f08b987cf8df04
Antoine <antoine.gagniere@orolia2s.com>
2024-04-23 09:30:39 +0200

    Add FNV1A macro

--- include/o2s/preprocessing.h
+++ include/o2s/preprocessing.h
@@ -15,11 +15,30 @@
  */
 /* ************************************************************************** */
 
+#include "o2s/private_preprocessing.h"
+
+/** @name Token manipulation */
+///@{
+
 /** Changes token type to string literal */
-#define LIBO2S_PREPRO_STRING_DIRECT(EXPRESSION) #EXPRESSION
+#define LIBO2S_PREPRO_STRING_DIRECT(TEXT) #TEXT
 
 /** Create a string literal from the evaluation of its argument */
-#define LIBO2S_PREPRO_STRING(EXPRESSION)        LIBO2S_PREPRO_STRING_DIRECT(EXPRESSION)
+#define LIBO2S_PREPRO_STRING(EXPRESSION)  LIBO2S_PREPRO_STRING_DIRECT(EXPRESSION)
+
+/** Evaluates as a single identifier token, that is @p TEXT1 concatenated with @p TEXT2 */
+#define LIBO2S_PREPRO_CONCAT_DIRECT(TEXT1, TEXT2) TEXT1 ## TEXT2
+
+/**
+ * Evaluates as a single identifier token,
+ * that is the evaluation of @p EXPRESSION1 concatenated with the evaluation of @p EXPRESSION2
+ */
+#define LIBO2S_PREPRO_CONCAT(EXPRESSION1, EXPRESSION2) LIBO2S_PREPRO_CONCAT_DIRECT(EXPRESSION1, EXPRESSION2)
+
+///@}
+
+/** @name Numbers manipulation */
+///@{
 
 /** Evaluates as the biggest of the two parameters */
 #define max(a, b) \
@@ -37,5 +56,17 @@
 		_a < _b ? _a : _b; \
 	})
 
+///@}
+
 /** Number of elements in a C array (on the stack) */
 #define C_ARRAY_LENGTH(ARRAY) (sizeof(ARRAY) / sizeof(*ARRAY))
+
+/** Evaluates as the number of arguments, up to 39 */
+#define LIBO2S_PREPRO_ARGUMENT_COUNT(...) O2SPP_ARGCNT1(__VA_ARGS__)
+
+/**
+ * Fowler–Noll–Vo hash function, at compile-time.
+ * Evaluates as an expression that will be computed at compile-time,
+ * representing the hash of the sequence of characters passed as arguments.
+ */
+#define FNV1A(...) FNV1A_N(__VA_ARGS__)
